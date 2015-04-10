f221da3c2b08dfa81672df20f6952e87bc6376ee
mariusz <budkagracza@gmail.com>
2015-04-10 02:30:31 +0200

    added basic exceptions

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.conf import settings
 from sorl.thumbnail import get_thumbnail
 import os
@@ -13,6 +13,14 @@ def index(request):
 
 def thumbnail(request, size):
     files = os.listdir(settings.MEDIA_ROOT)
+    if len(files) < 1:
+        return Http404("brak zdjec w folderze")
     picture = random.choice(files)
     im = get_thumbnail(picture, size, crop='center', quality=99)
-    return HttpResponseRedirect(im.url)
+    try:
+        readed_image = im.read()
+    except IOError:
+        return Http404("blad dostepu do pliku")
+    except Exception:
+        return Http404("nieokreslony blad")
+    return HttpResponse(readed_image, content_type="image/jpeg")
