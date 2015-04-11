0feb6a1e48781a92534d76d02b6e074e93ccf4cc
mariusz <budkagracza@gmail.com>
2015-04-11 13:04:19 +0200

    add tests for GET parameter

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+from .views import my_view
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get_parameters(self):
+        request_invalid_size = self.factory.get('/galeria/details')
+        response = my_view(request_invalid_size)
+        self.assertEqual(response.status_code, 404)
+
+        request_one_size = self.factory.get('/galeria/400')
+        response = my_view(request_one_size)
+        self.assertEqual(response.status_code, 404)
+        
+        request_one_size = self.factory.get('/galeria/400x4r5')
+        response = my_view(request_one_size)
+        self.assertEqual(response.status_code, 404)
