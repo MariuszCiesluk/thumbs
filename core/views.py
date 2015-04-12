8a1899d927dedf2c868c0081a0f384ed1f5c13ce
mariusz <budkagracza@gmail.com>
2015-04-13 00:45:29 +0200

    modified checking parameters and description for view

--- core/views.py
+++ core/views.py
@@ -19,9 +19,18 @@ def check_size(size):
     values = size.split('x')
     if len(values) < 2:
         raise Http404("brak dwoch wymiarow obrazka")
-    digits = re.compile('\d')
+    first_number = values[0].isdigit()
+    second_number = values[1].isdigit()
+    if not first_number:
+        raise Http404("pierwszy wymiar nie jest liczba")
+    if not second_number:
+        raise Http404("drugi wymiar nie jest liczba")
 
 def thumbnail(request, size):
+    """
+    main function for getting image from folder, cropping
+    and making response
+    """
     files = os.listdir(settings.MEDIA_ROOT)
     check_size(size)
     if len(files) < 1 or ('cache' in files and len(files) == 1):
